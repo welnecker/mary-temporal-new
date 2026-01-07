9fc981885671968ddf3a4c3e1f0edf45858d158f
welnecker <janiofdr@gmail.com>
2026-01-07 09:18:51 -0300

    Update service_cumplice.py

--- characters/mary/service_cumplice.py
+++ characters/mary/service_cumplice.py
@@ -1,1 +1,30 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from .service_core import MaryService as _MaryServiceCore
+
+
+class MaryServiceCumplice(_MaryServiceCore):
+    """
+    Wrapper: força timeline = 'cumplice'
+    """
+
+    def reply(
+        self,
+        user: str,
+        model: str,
+        *,
+        prompt: Optional[str] = None,
+        timeline: Optional[str] = None,
+        nsfw: Optional[bool] = None,
+    ) -> str:
+        # força timeline
+        return super().reply(
+            user=user,
+            model=model,
+            prompt=prompt,
+            timeline="cumplice",
+            nsfw=nsfw,
+        )
 
