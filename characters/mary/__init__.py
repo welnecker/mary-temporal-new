96b72f9e5cc537083d50ac221c8669d35604b1b2
welnecker <janiofdr@gmail.com>
2026-01-07 09:42:43 -0300

    Update __init__.py

--- characters/mary/__init__.py
+++ characters/mary/__init__.py
@@ -1,1 +1,13 @@
-__all__ = []
+# characters/mary/__init__.py
+from __future__ import annotations
+
+__all__ = [
+    # compat / uso comum
+    "persona",
+    "service",
+    # opcionais (wrappers)
+    "service_core",
+    "service_universitaria",
+    "service_cumplice",
+]
+
