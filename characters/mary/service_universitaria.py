5fc810735f11b9fafa762ce7ba70c2669f891205
welnecker <janiofdr@gmail.com>
2026-01-07 09:18:27 -0300

    Update service_universitaria.py

--- characters/mary/service_universitaria.py
+++ characters/mary/service_universitaria.py
@@ -1,1 +1,30 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from .service_core import MaryService as _MaryServiceCore
+
+
+class MaryServiceUniversitaria(_MaryServiceCore):
+    """
+    Wrapper: força timeline = 'universitaria'
+    """
+
+    def reply(
+        self,
+        user: str,
+        model: str,
+        *,
+        prompt: Optional[str] = None,
+        timeline: Optional[str] = None,
+        nsfw: Optional[bool] = None,
+    ) -> str:
+        # força timeline
+        return super().reply(
+            user=user,
+            model=model,
+            prompt=prompt,
+            timeline="universitaria",
+            nsfw=nsfw,
+        )
 
