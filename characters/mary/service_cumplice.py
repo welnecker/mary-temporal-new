c8f214d900293718dba678c90c5b643123a0e0af
welnecker <janiofdr@gmail.com>
2026-03-06 22:11:29 -0300

    Update service_cumplice.py

--- characters/mary/service_cumplice.py
+++ characters/mary/service_cumplice.py
@@ -1,3 +1,11 @@
+from __future__ import annotations
+
+from typing import Optional, Any
+from .service_core import MaryService
+
+
+class MaryServiceCumplice(MaryService):
+
 def reply(
     self,
     *,
