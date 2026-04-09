39a12d0462effe4ddcd7850debc52cf563399801
welnecker <janiofdr@gmail.com>
2026-04-09 15:27:12 -0300

    Update service_core.py

--- characters/mary/service_core.py
+++ characters/mary/service_core.py
@@ -6748,8 +6748,8 @@ Resumo:
    {nsfw_block}
    """.strip()
     
-    # 👇 MOSTRAR NA TELA
-    st.text_area("SYSTEM PROMPT", system, height=500)
+        # 👇 MOSTRAR NA TELA
+        st.text_area("SYSTEM PROMPT", system, height=500)
 
         system = (
             system.rstrip()
