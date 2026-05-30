3a066dcd1ffce57d0da99452c9c426a1ce98a94f
welnecker <janiofdr@gmail.com>
2026-05-30 13:41:58 -0300

    Update Teste_Mary_Minimo.py

--- pages/Teste_Mary_Minimo.py
+++ pages/Teste_Mary_Minimo.py
@@ -10,10 +10,10 @@ import base64
 import tempfile
 
 import streamlit as st
-import gspreadaplicar_estilo_sidebar_controles(
+import gspread
 from google.oauth2.service_account import Credentials
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))aplicar_estilo_sidebar_controles(
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
